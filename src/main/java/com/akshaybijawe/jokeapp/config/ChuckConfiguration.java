13cff834921c81a0877fde4f55e1f8b1564e7c3f
Akshay Bijawe <akshaybijawe121@gmail.com>
2018-03-04 14:40:20 -0800

    Refactor the code to add the Java Configuration of Spring Bean

---
+++ src/main/java/com/akshaybijawe/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,20 @@
+package com.akshaybijawe.jokeapp.config;
+
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author arbijawe on 3/4/18
+ * @project joke-app
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+
+}
